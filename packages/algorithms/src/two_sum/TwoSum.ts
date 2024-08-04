fd17ec153666eee4536befd3baf29293bd9a17a9
facinick <facinick@gmail.com>
2024-08-04 19:05:16 +0530

    Two sum using map

---
+++ packages/algorithms/src/two_sum/TwoSum.ts
@@ -0,0 +1,37 @@
+import { BinarySearch } from "../binary_search";
+
+// V1, O(N^2)
+// function twoSum(nums: number[], target: number): number[] {
+
+//     // because findIndex finds first index, in case of same valued pair we want index of both
+//     for(let i = nums.length - 1; i >= 0; i--) {
+//         const otherIndex = nums.findIndex(num => num === target-nums[i])
+//         if(otherIndex !== -1) {
+//             return [i, otherIndex]
+//         }
+//     }
+// }
+
+// V2, O(N)
+function TwoSum(nums: number[], target: number): number[] {
+
+    if(nums.length < 2) {
+        throw new Error(`Array must have atleast 2 elements`)
+    }
+
+    const numMap = new Map<number, number>()
+
+    for(let i=0; i<nums.length; i++) {
+        if(numMap.has(target - nums[i])) {
+            return [numMap.get(target - nums[i])!, i]
+        } else {
+            numMap.set(nums[i], i)
+        }
+    }
+
+    throw new Error(`No pair found that sums to ${target}`)
+}
+
+export {
+    TwoSum
+}
