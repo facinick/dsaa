7538358103ec6cdee58888b905a52f589a43d36b
facinick <facinick@gmail.com>
2024-08-06 16:16:28 +0530

    isAnagram

---
+++ packages/algorithms/src/valid_anagram/ValidAnagram.ts
@@ -0,0 +1,35 @@
+function isAnagram(s: string, t: string): boolean {
+
+    if(s.length !== t.length) {
+        return false
+    }
+
+    const characterMap: Record<string, number> = {}
+
+    for(let i=0; i<s.length; i++) {
+        if(characterMap[s.at(i)!]) {
+            characterMap[s.at(i)!]++
+        } else {
+            characterMap[s.at(i)!] = 1
+        }
+    }
+
+    for(let i=0; i<t.length; i++) {
+
+        if(characterMap[t.at(i)!] === undefined) {
+            return false
+        }
+
+        if(characterMap[t.at(i)!] > 1) {
+            characterMap[t.at(i)!]--
+        } else {
+            delete characterMap[t.at(i)!]
+        }
+    }
+
+    return Object.keys(characterMap).length === 0
+};
+
+export {
+    isAnagram
+}
---
+++ packages/algorithms/src/valid_anagram/index.ts
@@ -0,0 +1,1 @@
+export {isAnagram} from './ValidAnagram'
