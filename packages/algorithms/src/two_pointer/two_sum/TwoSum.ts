8aa864c01b963017f66c5f95aa05034203bebe82
facinick <facinick@gmail.com>
2024-08-08 14:47:15 +0530

    twoSum

---
+++ packages/algorithms/src/two_pointer/two_sum/TwoSum.ts
@@ -0,0 +1,20 @@
+function twoSum(numbers: number[], target: number): number[] {
+    
+    let i = 0
+    let j = numbers.length - 1
+
+    // exactly one solution exists, and those elements are different so
+    while(numbers[i] + numbers[j] !== target) {
+        if(numbers[i] + numbers[j] < target) {
+            i++
+        } else {
+            j--
+        }
+    }
+
+    return [i+1, j+1]
+};
+
+export {
+    twoSum
+}
