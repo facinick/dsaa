5a0722af829393d118010b47bffc48bfb363fdd7
facinick <facinick@gmail.com>
2024-08-15 17:37:41 +0530

    productExceptSelf

---
+++ packages/algorithms/src/dynamic_programming/product_except_self/ProductExceptSelf.ts
@@ -0,0 +1,23 @@
+function productExceptSelf(nums: number[]): number[] {
+
+    const prefProd: number[] = []
+    const suffProd: number[] = []
+    const prodExceptSelf = []
+
+    for(let i=0; i<nums.length; i++) {
+        let leftIndex = i
+        let rightIndex = nums.length - i - 1
+        prefProd[leftIndex] = i === 0 ? 1 : prefProd[leftIndex-1] * nums[leftIndex-1]
+        suffProd[rightIndex] = i === 0 ? 1 : suffProd[rightIndex+1] * nums[rightIndex+1]
+    }
+
+    for(let i=0; i<nums.length; i++) {
+        prodExceptSelf[i] = prefProd[i] * suffProd[i]
+    }
+
+    return prodExceptSelf
+};
+
+export {
+    productExceptSelf
+}
