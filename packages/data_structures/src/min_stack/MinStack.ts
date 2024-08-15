ac9253993fa28b50ba5b21e02108613631e9d6f7
facinick <facinick@gmail.com>
2024-08-16 00:17:14 +0530

    MinStack

---
+++ packages/data_structures/src/min_stack/MinStack.ts
@@ -0,0 +1,44 @@
+class MinStack {
+
+    private stack: Array<number> = new Array()
+    private _top: number = 0
+
+    private minimumPrefixStack: Array<number> = new Array()
+
+    constructor() {
+        
+    }
+
+    push(val: number): void {
+        this.stack[this._top] = val
+        this.minimumPrefixStack[this._top] = this._top === 0? val: Math.min(this.minimumPrefixStack[this._top-1], val)
+        
+        this._top++
+    }
+
+    pop(): void {
+        if(this._top === 0) {
+            throw new Error(`Stack is empty`)
+        }
+        --this._top
+        return
+    }
+
+    top(): number {
+        if(this._top === 0) {
+            throw new Error(`Stack is empty`)
+        }
+        return this.stack[this._top-1]
+    }
+
+    getMin(): number {
+        if(this._top === 0) {
+            throw new Error(`Stack is empty`)
+        }
+        return this.minimumPrefixStack[this._top-1]
+    }
+}
+
+export {
+    MinStack
+}
