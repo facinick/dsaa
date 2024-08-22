445f809eb4eec662099dca239741cd1361402152
facinick <facinick@gmail.com>
2024-08-23 00:21:43 +0530

    updated title

--- packages/animations/src/components/Dashboard/Dashboard.tsx
+++ packages/animations/src/components/Dashboard/Dashboard.tsx
@@ -23,7 +23,7 @@ export function Dashboard() {
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link to="/" className="flex items-center gap-2 font-semibold">
               <ChartNetwork className="h-6 w-6" />
-              <span className="">DSA a</span>
+              <span className="">DSAA by @facinick</span>
             </Link>
             {/* <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
